e9702c6257a94574dfde7e2120839d35aa5c2a7c
ramazan <ramazan.kus@etu.univ-lyon1.fr>
2023-10-20 11:54:37 +0200

    categorie store

--- store.php
+++ store.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <header>
-        <a id="accueil" href="actu.html"><img src="logo.png"></a>
+        <a id="accueil" href="actu.html"><img src="images/logo.png"></a>
         <a id="actu" href="">Actu</a>
         <a id="prono" href="">Prono</a>
         <a id="boutique" href="">Boutique</a>
