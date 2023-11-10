c78d810d0158ebb7aceb31a54dcc9a6828e95b96
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2023-11-10 15:47:06 +0100

    rearrangement du code

--- inscription/index.js
+++ inscription/index.js
@@ -1,36 +1,30 @@
-let btnRight = document.querySelector(".right");
-btnRight.addEventListener("click", slideDroite)
-function slideDroite() {
-    alert ("right!");
-  }
-  function slideGauche() {
-    alert ("left!");
-  }
-  let btnLeft = document.querySelector(".left");
-  btnLeft.addEventListener("click", slideGauche)
 
-//slide
-  let slideIndex = 1;
-showSlides(slideIndex);
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName(".pos-nom");
-  
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
- 
-  slides[slideIndex-1].style.display = "block";  
-  
-}
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
