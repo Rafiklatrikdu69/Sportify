cb042e407631dcf9f39a2d8a673a4643c7a7bf0e
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2023-11-18 19:41:10 +0100

    resolution conflit

--- app/views/index.php
+++ app/views/index.php
@@ -9,17 +9,11 @@ use \App\Config;
     <link rel="stylesheet" href="css/style.css">
     <title>Sportify</title>
 </head>
-<<<<<<< HEAD:accueuil/index.html
-<body>
-    <header>
-        <h1>Sportify</h1>
-    </header>
-        <main>
-=======
+
 <?php echo $name;?>
 <h1>Sportify</h1>
 <main>
->>>>>>> feature/formulaire:app/views/index.php
+
             <!-- Partie gauche -->
                 <div class="div1">
                     <img src="images/logo.png" alt="logo">
@@ -33,22 +27,18 @@ use \App\Config;
                     </div>
                     <div class="div4">
                         <p>Rejoignez nous maintenant !</p>
-<<<<<<< HEAD:accueuil/index.html
-                    <button onclick="window.location.href='../inscription/inscription.html';" class="form-control btn btn-primary submit px-3" role="button">S'inscrire</button>
-=======
+
                     <button onclick="window.location.href='/public/inscription';" class="form-control btn btn-primary submit px-3" role="button">S'inscrire</button>
->>>>>>> feature/formulaire:app/views/index.php
+
                     </div>
                     <div class="div5">
                         <h2>Ou</h2>
                     </div>
                     <div class="div6">
                         <p>Deja un sportifyer</p>
-<<<<<<< HEAD:accueuil/index.html
-                        <button onclick="window.location.href='../connexion/index.html';" class="form-control btn btn-primary submit px-3" role="button">Se connecter</button>
-=======
+
                         <button onclick="window.location.href='/public/connexion';" class="form-control btn btn-primary submit px-3" role="button">Se connecter</button>
->>>>>>> feature/formulaire:app/views/index.php
+
                     </div>
                 </div>
         </main>
