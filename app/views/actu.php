7bb775e8258ad928cda4520a0c3805dd349ff69a
Keap <haithemly69@gmail.com>
2023-12-05 15:43:09 +0100

    header dynamique

--- app/views/actu.php
+++ app/views/actu.php
@@ -8,13 +8,7 @@
     
 </head>
 <body>
-    <header>
-        <a id="accueil" href="actu.html"><img src="../../public/images/logo.png"></a>
-        <a id="actu" href="">Actu</a>
-        <a id="prono" href="">Prono</a>
-        <a id="boutique" href="">Boutique</a>
-        <a id="deconnexion" href="">Déconnexion</a>
-    </header>
+<?php header::header()?>
     <div id="base">
     <aside>
         <button id="openModal"> Actualité</button>
