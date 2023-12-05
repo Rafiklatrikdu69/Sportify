9c6f0f39ea1edf12c2cf098b507c34f161a3ea0f
RAHMANI NAHIL p2208544 <nahil.rahmani@etu.univ-lyon1.fr>
2023-12-05 13:20:38 +0000

    ModelScore

---
+++ SportyJump/Model/Score.js
@@ -0,0 +1,47 @@
+var nbScore = 0;
+var nbPiece = 0;
+var point = 0;
+var pointGagne = 40 / getFacteur();
+var activerScore = false;
+var score = 0
+const scoreTexte = document.getElementById("score");
+const nbPieceTexte = document.getElementById("nbPiece");
+function setPointGagner(new_valeur) {
+    point = new_valeur;
+}
+function setPoint() {
+    if (point > 0) {
+        point = point + pointGagne;
+    } else {
+        point = pointGagne;
+    }
+}
+function diminuerPoint() {
+    point = point - 1;
+}
+function augmenterScore() {
+    score = score + point;
+    scoreTexte.innerHTML = score;
+}
+function augmenterNbPiece() {
+    nbPiece = nbPiece + 1;
+    nbPieceTexte.innerHTML = "×" + nbPiece;
+}
+function ActivationScore() {
+    activerScore = true;
+    setPoint();
+}
+function makeScore() {
+    if (activerScore) {
+        augmenterScore();
+        diminuerPoint();
+        if (point <= 0) {
+            activerScore = false;
+        }
+    }
+}
+
+function TransfererScore() {
+    localStorage.setItem("scoreTexte", JSON.stringify(score));
+    localStorage.setItem("nbPieceTexte", JSON.stringify(nbPiece));
+}
