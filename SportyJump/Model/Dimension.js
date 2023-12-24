11d2b698f08afe94644a5e890ea6eab2bafb8211
p2208544 <nahil.rahmani@etu.univ-lyon1.fr>
2023-12-24 11:18:53 +0100

    Fin du tuto Menu et Gameplay, dev du défilement du tuto et rajout de trois autres tuto (5 en tout)

--- SportyJump/Model/Dimension.js
+++ SportyJump/Model/Dimension.js
@@ -67,8 +67,21 @@ function chbgout() {
 
 
 const tuto = document.getElementById("circle");
-document.querySelector(".tutoType").classList.add("invisible"); 
-var tutoClick = false; 
+const rect = document.getElementById("rectangle");
+var timerTuto;
+var timerDefilementTutoD;
+var timerDefilementTutoG;
+var tutoType = document.querySelector(".tutoType");
+tutoType.classList.add("invisible");
+tutoType.style.position = "absolute";
+var tutoClick = false;
+var numMenu = 1;
+var cirlce = [];
+var tutoClass = [];
+var placement = 10;
+var vitesseTuto = 60;
+
+document.querySelector(".cercle_Tuto").style.position = "absolute";
 
 tuto.addEventListener("mouseover", function () {
     tuto.style.transitionDuration = "1s";
@@ -80,66 +93,233 @@ tuto.addEventListener("mouseout", function () {
     tuto.style.transitionDuration = "1s";
     document.getElementById("point").style.color = "#fff";
     document.getElementById("circle").style.backgroundColor = "#000";
-    bloque(); 
+    bloque();
 });
 tuto.addEventListener("click", function () {
-    stopTimerDefilementTuto(); 
+    stopTimerDefilementTuto();
     tutoClick = !tutoClick;
-    if(tutoClick){
+    if (tutoClick) {
         debloque();
-        document.querySelector(".tutoType").classList.remove("invisible"); 
+        tutoType.classList.remove("invisible");
     }
     startTimerDefilementTuto();
 });
-document.querySelector(".interfaceJeu").addEventListener("click", function(){
-        stopTimerDefilementTuto(); 
-        tutoClick = false; 
-        startTimerDefilementTuto();
-}); 
-document.querySelector("header").addEventListener("click", function(){
-    stopTimerDefilementTuto(); 
-    tutoClick = false; 
+document.querySelector(".interfaceJeu").addEventListener("click", function () {
+    stopTimerDefilementTuto();
+    tutoClick = false;
     startTimerDefilementTuto();
-}); 
+});
+document.querySelector("header").addEventListener("click", function () {
+    stopTimerDefilementTuto();
+    tutoClick = false;
+    startTimerDefilementTuto();
+});
 
 function debloque() {
     var rectangle = document.getElementById("rectangle");
     rectangle.classList.add("expanded");
 }
 
-function bloque(){
+function bloque() {
     var rectangle = document.getElementById("rectangle");
-    if(!tutoClick){
+    if (!tutoClick) {
         rectangle.classList.remove("expanded");
     }
 }
-
-const rect = document.getElementById("rectangle");
-var timerTuto;
 
 function startTimerDefilementTuto() {
     timerTuto = setInterval(function () {
         if (tutoClick) {
             if (rect.offsetLeft >= 0) {
-                stopTimerDefilementTuto(); 
+                stopTimerDefilementTuto();
                 rectangle.classList.add("expanded");
             } else {
-                rect.style.left = rect.offsetLeft + 25 + "px";
+                rect.style.left = rect.offsetLeft + 30 + "px";
             }
         } else {
             if (rect.offsetLeft <= -690) {
-                stopTimerDefilementTuto(); 
+                stopTimerDefilementTuto();
                 rect.style.left = -690 + "px";
-                document.querySelector(".tutoType").classList.add("invisible"); 
+                tutoType.classList.add("invisible");
                 rectangle.classList.remove("expanded");
             } else {
-                rect.style.left = rect.offsetLeft - 25 + "px";
+                rect.style.left = rect.offsetLeft - 30 + "px";
             }
         }
     }, getRafraichissement());
 }
-function stopTimerDefilementTuto(){
-    clearInterval(timerTuto); 
+function stopTimerDefilementTuto() {
+    clearInterval(timerTuto);
+}
+
+function cercleTutoMenu() {
+    for (let i = 0; i < 5; i++) {
+        var cercle = document.createElement("div");
+        cercle.id = "cercle" + i;
+        cercle.style.width = "10px";
+        cercle.style.height = "10px";
+        cercle.style.backgroundColor = "#fff";
+        cercle.style.borderRadius = "50%";
+        cercle.style.transition = "background-color 0.5s, color 0.5s";
+        document.querySelector(".cercle_Tuto").appendChild(cercle);
+        cirlce.push(cercle);
+    }
+}
+function flecheTutoMenu() {
+    var fleche1 = document.createElement("img");
+    fleche1.src = "Dessin/Menu/boutonDroitePresse.png";
+    fleche1.style.position = "absolute";
+    fleche1.style.left = "93%";
+    fleche1.style.top = "40%";
+    fleche1.id = "flecheD";
+    fleche1.style.opacity = "0";
+    fleche1.style.transition = "opacity 0.3s";
+    document.querySelector(".fleche").appendChild(fleche1);
+    var fleche2 = document.createElement("img");
+    fleche2.src = "Dessin/Menu/boutonGauchePresse.png";
+    fleche2.style.position = "absolute";
+    fleche2.style.left = "0%";
+    fleche2.style.top = "40%";
+    fleche2.id = "flecheG";
+    fleche2.style.opacity = "0";
+    fleche2.style.transition = "opacity 0.3s";
+    document.querySelector(".fleche").appendChild(fleche2);
+}
+function ActionFleche() {
+    var flecheD = document.getElementById("flecheD");
+    var flecheG = document.getElementById("flecheG");
+    flecheD.addEventListener("mouseover", function () {
+        flecheD.style.opacity = "1";
+    });
+    flecheD.addEventListener("mouseout", function () {
+        flecheD.style.opacity = "0";
+    });
+    flecheG.addEventListener("mouseover", function () {
+        flecheG.style.opacity = "1";
+    });
+    flecheG.addEventListener("mouseout", function () {
+        flecheG.style.opacity = "0";
+    });
+    flecheD.addEventListener("click", function () {
+        switch (numMenu) {
+            case 1:
+                numMenu = 2;
+                cirlce[0].style.backgroundColor = "#fff";
+                cirlce[1].style.backgroundColor = "#000";
+                break;
+            case 2:
+                numMenu = 3;
+                cirlce[1].style.backgroundColor = "#fff";
+                cirlce[2].style.backgroundColor = "#000";
+                break;
+            case 3:
+                numMenu = 4;
+                cirlce[2].style.backgroundColor = "#fff";
+                cirlce[3].style.backgroundColor = "#000";
+                break;
+            case 4:
+                numMenu = 5;
+                cirlce[3].style.backgroundColor = "#fff";
+                cirlce[4].style.backgroundColor = "#000";
+                break;
+        }
+        stopTimerDefilementTutoDroite();
+        startTimerDefilementTutoDroite();
+    });
+    flecheG.addEventListener("click", function () {
+        switch (numMenu) {
+            case 2:
+                numMenu = 1;
+                cirlce[1].style.backgroundColor = "#fff";
+                cirlce[0].style.backgroundColor = "#000";
+                break;
+            case 3:
+                numMenu = 2;
+                cirlce[2].style.backgroundColor = "#fff";
+                cirlce[1].style.backgroundColor = "#000";
+                break;
+            case 4:
+                numMenu = 3;
+                cirlce[3].style.backgroundColor = "#fff";
+                cirlce[2].style.backgroundColor = "#000";
+                break;
+            case 5:
+                numMenu = 4;
+                cirlce[4].style.backgroundColor = "#fff";
+                cirlce[3].style.backgroundColor = "#000";
+                break;
+        }
+        stopTimerDefilementTutoGauche();
+        startTimerDefilementTutoGauche();
+    });
+}
+function startTimerDefilementTutoDroite() {
+    timerDefilementTutoD = setInterval(function () {
+        tutoType.style.left = tutoType.offsetLeft - vitesseTuto + "px";
+        switch (numMenu) {
+            case 2:
+                if (tutoType.offsetLeft < -700 - 100) {
+                    stopTimerDefilementTutoDroite();
+                    tutoType.style.left = -700 - 100 + 10 + "px";
+                }
+                break;
+            case 3:
+                if (tutoType.offsetLeft < (-700 - 100) * 2) {
+                    stopTimerDefilementTutoDroite();
+                    tutoType.style.left = (-700 - 100) * 2 + 10 + "px";
+                }
+                break;
+            case 4:
+                if (tutoType.offsetLeft < (-700 - 100) * 3) {
+                    stopTimerDefilementTutoDroite();
+                    tutoType.style.left = (-700 - 100) * 3 + 10 + "px";
+                }
+                break;
+            case 5:
+                if (tutoType.offsetLeft < (-700 - 100) * 4) {
+                    stopTimerDefilementTutoDroite();
+                    tutoType.style.left = (-700 - 100) * 4 + 10 + "px";
+                }
+                break;
+        }
+    }, getRafraichissement());
+}
+function stopTimerDefilementTutoDroite() {
+    clearInterval(timerDefilementTutoD);
+}
+function startTimerDefilementTutoGauche() {
+    timerDefilementTutoG = setInterval(function () {
+        tutoType.style.left = tutoType.offsetLeft + vitesseTuto + "px";
+        switch (numMenu) {
+            case 1:
+                if (tutoType.offsetLeft > 10) {
+                    stopTimerDefilementTutoGauche();
+                    tutoType.style.left = 10 + "px";
+                }
+                break;
+            case 2:
+                if (tutoType.offsetLeft > -700 - 100) {
+                    stopTimerDefilementTutoGauche();
+                    tutoType.style.left = -700 - 100 + "px";
+                }
+                break;
+            case 3:
+                if (tutoType.offsetLeft > (-700 - 100) * 2) {
+                    stopTimerDefilementTutoGauche();
+                    tutoType.style.left = (-700 - 100) * 2 + "px";
+                }
+                break;
+            case 4:
+                if (tutoType.offsetLeft > (-700 - 100) * 3) {
+                    stopTimerDefilementTutoGauche();
+                    tutoType.style.left = (-700 - 100) * 3 + "px";
+                }
+                break;
+        }
+    }, getRafraichissement());
+}
+function stopTimerDefilementTutoGauche() {
+    clearInterval(timerDefilementTutoG);
 }
 function TutoMenu() {
     var tuto = document.querySelector(".tutoMenu")
@@ -147,10 +327,10 @@ function TutoMenu() {
     titre.textContent = "Fonctionnement du Menu";
     titre.classList.add("titre");
     var text = document.createElement("a");
-    text.textContent = 'Cliqué sur le bouton "Jouer" pour commencer une partie ou sur le bouton "Classement" pour connaitre votre rang.';
+    text.textContent = 'Cliquez sur le bouton "Jouer" pour commencer une partie ou sur le bouton "Classement" pour connaitre votre rang.';
     text.classList.add("text");
     var text2 = document.createElement("a");
-    text2.textContent = "Vous pouvez aussi changer le thème du jeu en choisissant différents sports :";
+    text2.textContent = "Vous pouvez aussi changer le thème du jeu en choisissant différents sports:";
     text2.classList.add("text");
     var gif = document.createElement("img");
     gif.src = "Dessin/Tuto/tutoMenu.gif";
@@ -167,34 +347,81 @@ function TutoMenu() {
     tuto.appendChild(document.createElement("br"));
     tuto.appendChild(text3);
 
-    var mode = []; 
-    for(let i=0; i<6; i++){
-        mode.push(document.createElement("a")); 
-        mode[i].classList.add("text"); 
+    var mode = [];
+    for (let i = 0; i < 6; i++) {
+        mode.push(document.createElement("a"));
+        mode[i].classList.add("text");
     }
-    mode[0].textContent = "Foot => Mode Normale (Mode le mieux équilibré, fonctionnalité classique)";  
+    mode[0].textContent = "Foot => Mode Normale (Mode le mieux équilibré, fonctionnalité classique)";
     mode[1].textContent = "Basket => Mode HardCore (Fréquence des monstres très élevés)"
-    mode[2].textContent = "Tennis => Mode Vitesse (Vitesse de la balle augmentées)"; 
-    mode[3].textContent = "Baseball => Mode Richesse (Fréquence des pieces très élevées)"; 
-    mode[4].textContent = "Rugby => Mode Difficile (Moins de plateformes, 12 au lieu de 20)"; 
-    mode[5].textContent = "Bowling => Mode Mouvement (Toutes les plateformes se déplacent)"; 
-    for(let i=0; i<mode.length; i++){
+    mode[2].textContent = "Tennis => Mode Vitesse (Vitesse de la balle augmentées)";
+    mode[3].textContent = "Baseball => Mode Richesse (Fréquence des pieces très élevées)";
+    mode[4].textContent = "Rugby => Mode Difficile (Moins de plateformes, 12 au lieu de 20)";
+    mode[5].textContent = "Bowling => Mode Mouvement (Toutes les plateformes se déplacent)";
+    for (let i = 0; i < mode.length; i++) {
         tuto.appendChild(document.createElement("br"));
         tuto.appendChild(mode[i]);
     }
-
 }
-function TutoGameplay() {
 
+function TutoGameplay() {
+    var tuto = document.querySelector(".tutoGamePlay");
+    var titre = document.createElement("p");
+    titre.textContent = "GamePlay";
+    titre.classList.add("titre");
+    var text = document.createElement("a");
+    text.textContent = "Deplacez la balle en appuyant sur les flèches directonnelles du clavier. Vous pouvez changer sa direction selon la direction choisis entre la droite et la gauche.";
+    text.classList.add("text");
+    var touche = document.createElement("img");
+    touche.src = "Dessin/Tuto/toucheClavier.gif";
+    touche.style.padding = "45px 45px";
+    var jeu = document.createElement("img");
+    jeu.style.position = "absolute";
+    jeu.src = "Dessin/Tuto/tutoGamePlay.gif";
+    jeu.style.left = "29%";
+    jeu.style.top = "105px";
+    jeu.style.border = "5px solid #000";
+    var text2 = document.createElement("a");
+    text2.textContent = "A chaques plateformes touchées et de saut effectué, le score augmente de 210 point. Des pieces, des monstres et plusieurs objets aves des fonctionnalités différentes sont éparpillés sur le terrain.";
+    text2.classList.add("text");
+    tuto.appendChild(titre);
+    tuto.appendChild(text);
+    tuto.appendChild(document.createElement("br"));
+    tuto.appendChild(touche);
+    tuto.appendChild(jeu);
+    tuto.appendChild(document.createElement("br"));
+    tuto.appendChild(text2);
 }
 function TutoPiece() {
+    var tuto = document.querySelector(".tutoPiece");
+    var titre = document.createElement("p");
+    titre.textContent = "Obtention des pieces";
+    titre.classList.add("titre");
 
+    tuto.appendChild(titre);
 }
 function TutoMonstre() {
+    var tuto = document.querySelector(".tutoMonstre");
+    var titre = document.createElement("p");
+    titre.textContent = "Monstre";
+    titre.classList.add("titre");
 
+    tuto.appendChild(titre);
 }
 function TutoObject() {
+    var tuto = document.querySelector(".tutoObject");
+    var titre = document.createElement("p");
+    titre.textContent = "Les Objets";
+    titre.classList.add("titre");
 
+    tuto.appendChild(titre);
 }
-
-TutoMenu(); 
+flecheTutoMenu();
+ActionFleche();
+cercleTutoMenu();
+document.getElementById("cercle0").style.background = "#000";
+TutoMenu();
+TutoGameplay();
+TutoPiece();
+TutoMonstre();
+TutoObject(); 
