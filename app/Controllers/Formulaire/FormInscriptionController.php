19f4019c9ca58d2a857473a3cd34a375bd218217
Keap <haithemly69@gmail.com>
2024-01-10 14:29:06 +0100

    Redirection

--- app/Controllers/Formulaire/FormInscriptionController.php
+++ app/Controllers/Formulaire/FormInscriptionController.php
@@ -2,44 +2,42 @@
 
 
 class FormInscriptionController extends DefaultFormController{
+    public function __construct() {
+    
+   
+    }
     public function verification(){
-        if($_SERVER['REQUEST_METHOD']=='POST'){
-                echo "post";
-
-        }
-        else{
-            echo "get";
-        }
-        echo $_POST['username'];
-        echo $_POST['email'];
-        echo $_POST['password'];
-        ob_start();
+        $userDAO = new UtilisateurDAO();
         if(!empty($_POST['username'])&& !empty($_POST['email']) && !empty($_POST['password'])&&$_SERVER['REQUEST_METHOD'] === 'POST'){
-            
             $nom =   Validate::html($_POST['username']);
             $email =  Validate::html($_POST['email']);
             $mdp =  Validate::html($_POST['password']);
             $emailProtect =     filter_var($email,FILTER_SANITIZE_EMAIL);
             if(filter_var($emailProtect,FILTER_VALIDATE_EMAIL)){
-            echo $nom . $emailProtect . $mdp;
             $pass_protect = password_hash($mdp, PASSWORD_DEFAULT);
-           
-            $select =  (new UtilisateurDAO())->selectInscription($nom,$pass_protect,$emailProtect);
+            $select =  $userDAO->selectInscription($nom,$pass_protect,$emailProtect);
             if(!$select){
                 $utilisateur = new Utilisateur(0,$nom,$emailProtect,$pass_protect,100,0,1,0);
-                (new UtilisateurDAO())->insertUtilisateur($utilisateur);
+                $userDAO->insertUtilisateur($utilisateur);
                 echo "insert reussi !";
-                
+                //Appel script js
+                echo "<script>
+                    window.location.replace('http://localhost/public/connexion');
+                </script>";
+ 
             }else{
-                Redirect::redirect('/public/inscription');
+                echo "insert echoué !";
+                echo "<script>
+                    window.location.replace('http://localhost/public/connexion');
+                </script>";
             }
-            Redirect::redirect('/public/connexion');
+           
            
         }
           
-        }else{
-            Redirect::redirect('/public/inscription');
         }
-        ob_end_clean();
+   
+     
+        
     }
 }
