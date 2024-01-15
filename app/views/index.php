d339562ba86ce8ed65fab33f563dd95604fa5b22
p2208544 <nahil.rahmani@etu.univ-lyon1.fr>
2024-01-15 18:20:52 +0100

    test

--- app/views/index.php
+++ app/views/index.php
@@ -11,37 +11,31 @@ use \App\Config;
 </head>
 
 <?php echo $name;?>
-<h1>Sportify</h1>
-<main>
 
-            <!-- Partie gauche -->
-                <div class="div1">
-                    <img src="images/logo.png" alt="logo">
-                </div>
-            <!-- Partie droite -->
-                <div class="div2">
-                    <div class="div3">
-                        <div class="slogan">
-                                <h2>C'est maintenant que ca se passe !</h2>
-                        </div>
-                    </div>
-                    <div class="div4">
-                        <p>Rejoignez nous maintenant !</p>
+<body>
+    <div class="barreMenu">
+        <div class="gridTitre">
+            <img class="logo" src="images/logo.png">
+            <a>Sportify</a>
+        </div>
+        <div class="gridProfil">
+            <a>Devenez un Sportifyer <b>|</b> </a>
+            <a>Profil</a>
+        </div>
+        
+    </div>
+    
+    <div class="accueil">
+        <div class="gauche">
+            <a>Plongez-vous dans l’expérience ultime des matches en lignes</a>
+        </div>
+        <div class="droite">
+        </div>
+    </div>
+    <button onclick="window.location.href='/public/inscription';" class="form-control btn btn-primary submit px-3" role="button">S'inscrire</button>         
+    <button onclick="window.location.href='/public/connexion';" class="form-control btn btn-primary submit px-3" role="button">Se connecter</button>
 
-                    <button onclick="window.location.href='/public/inscription';" class="form-control btn btn-primary submit px-3" role="button">S'inscrire</button>
-
-                    </div>
-                    <div class="div5">
-                        <h2>Ou</h2>
-                    </div>
-                    <div class="div6">
-                        <p>Deja un sportifyer</p>
-
-                        <button onclick="window.location.href='/public/connexion';" class="form-control btn btn-primary submit px-3" role="button">Se connecter</button>
-
-                    </div>
-                </div>
-        </main>
+</body>
     <footer>
         <p>&copy; Sportify 2023</p>
         <p>Site réalisé dans le cadre de la SAE S3</p>
